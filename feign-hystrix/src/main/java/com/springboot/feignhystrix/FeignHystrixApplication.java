a21779af960645c21b99f6141e2a03b286616c01
Phoenix-zhy <1262312629@qq.com>
2020-01-07 22:50:57 +0800

    init

---
+++ feign-hystrix/src/main/java/com/springboot/feignhystrix/FeignHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.feignhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignHystrixApplication.class, args);
+	}
+
+}
